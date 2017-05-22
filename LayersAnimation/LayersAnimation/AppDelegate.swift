e160d60df8942a0c0ff1b4b39817e74f564841dd
Mazy <mazy_ios@163.com>
2017-05-22 18:01:44 +0800

    feat: 配置全局属性,添加 UIImage切图分类

--- LayersAnimation/LayersAnimation/AppDelegate.swift
+++ LayersAnimation/LayersAnimation/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+// 60 208 221
+
+let globalColor = UIColor(red: 60/255, green: 208/255, blue: 221/255, alpha: 1.0)
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,32 +18,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // 设置 UILabel 的全局属性
+        UILabel.appearance().font = UIFont(name: "Avenir-Light", size: 17.0)
+        UILabel.appearance(whenContainedInInstancesOf: [UITableViewCell.self]).font = UIFont(name: "Avenir-Light", size: 14.0)
+        // 设置导航栏属性
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().barTintColor = globalColor
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white,NSFontAttributeName: UIFont(name: "Avenir-Light", size: 20.0)!]
+        // 设置 tableView 的全局属性
+        UITableView.appearance().separatorColor = globalColor
+        // 设置 tableViewCell的分割线偏移为0
+        UITableViewCell.appearance().separatorInset = UIEdgeInsets.zero
+        // 设置所有 UIControl 的 颜色为全局色
+        UIControl.appearance().tintColor = globalColor
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
