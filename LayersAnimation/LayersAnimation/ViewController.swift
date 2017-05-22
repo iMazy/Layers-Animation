1f491672d913c4a563ebbc772229932602416b9e
Mazy <mazy_ios@163.com>
2017-05-22 18:13:17 +0800

    feat: 完成首页的展示

--- LayersAnimation/LayersAnimation/ViewController.swift
+++ LayersAnimation/LayersAnimation/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    /// 数组中存放元祖 title and subTitle
+    var classes: [(String, String)] {
+        get {
+            return [
+                ("CALayer", "Manage and animate visual content"),
+                ("CAScrollLayer", "Display portion of a scrollable layer"),
+                ("CATextLayer", "Render plain text or attributed strings"),
+                ("AVPlayerLayer", "Display an AV player "),
+                ("CAGradientLayer", "Apply a color gradient over the background"),
+                ("CAReplicatorLayer", "Duplicate a source layer"),
+                ("CATiledLayer", "Asynchronously draw layer content in tiles"),
+                ("CAShapeLayer", "Draw using scalable vector paths"),
+                ("CAEAGLLayer", "Draw OpenGL content"),
+                ("CATransformLayer", "Draw 3D structures"),
+                ("CAEmitterLayer", "Render animated particles")
+            ]
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = 60
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return classes.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "ClassCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "ClassCell")
+            cell?.accessoryType = .disclosureIndicator
+        }
+        let row = indexPath.row
+        cell?.textLabel?.text = classes[row].0
+        cell?.detailTextLabel?.text = classes[row].1
+        return cell!
+    }
 }
 
