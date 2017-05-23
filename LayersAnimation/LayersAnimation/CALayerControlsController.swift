fe3d0e2358023c31ed0f86dd73b7a6507eadff46
Mazy <mazongyong@163.com>
2017-05-23 21:48:03 +0800

    feat: 完成视图与SB的连线

--- LayersAnimation/LayersAnimation/CALayerControlsController.swift
+++ LayersAnimation/LayersAnimation/CALayerControlsController.swift
@@ -10,88 +10,46 @@ import UIKit
 
 class CALayerControlsController: UITableViewController {
     
+    @IBOutlet weak var contentsGravityPickerValueLabel: UILabel!
+    @IBOutlet weak var contentsGravityPicker: UIPickerView!
+    @IBOutlet var switches: [UISwitch]!
+    @IBOutlet var sliderValueLabels: [UILabel]!
+    @IBOutlet var sliders: [UISlider]!
+    @IBOutlet weak var borderColorSlidersValueLabel: UILabel!
+    @IBOutlet var borderColorSliders: [UISlider]!
+    @IBOutlet weak var backgroundColorSlidersValueLabel: UILabel!
+    @IBOutlet var backgroundColorSliders: [UISlider]!
+    @IBOutlet weak var shadowOffsetSlidersValueLabel: UILabel!
+    @IBOutlet var shadowOffsetSliders: [UISlider]!
+    @IBOutlet weak var shadowColorSlidersValueLabel: UILabel!
+    @IBOutlet var shadowColorSliders: [UISlider]!
+    @IBOutlet weak var magnificationFilterSegmentedControl: UISegmentedControl!
+    
     weak var layerViewController: CALayerViewController!
+    
+    enum row: Int {
+        case contentsGravity, contentsGravityPicker, displayContents, geometryFlipped, hidden, opacity, cornerRadius, borderWidth, borderColor, backgroundColor, shadowOpacity, shadowOffset, shadowRadius, shadowColor, magnificationFilter
+    }
+    
+    enum Switch: Int {
+        case opacity, cornerRadius, borderWidth, shadowOpacity, shadowRadius
+    }
+    
+    enum ColorSlider: Int {
+        case red, green, blue
+    }
+    
+    enum MagnificationFilter: Int {
+        case linear, nearest, trilinear
+    }
+    
+    var contentsGravityValues = [kCAGravityCenter, kCAGravityTop, kCAGravityBottom, kCAGravityLeft, kCAGravityRight, kCAGravityTopLeft, kCAGravityTopRight, kCAGravityBottomLeft, kCAGravityBottomRight, kCAGravityResize, kCAGravityResizeAspect, kCAGravityResizeAspectFill] as NSArray
+    
+    var contentsGravityPickerVisible = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+       
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
