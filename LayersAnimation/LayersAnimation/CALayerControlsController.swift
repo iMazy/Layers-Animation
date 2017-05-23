4be9cd9d911cdb4c9a2912ff4e2c1cda9e9db6bc
Mazy <mazongyong@163.com>
2017-05-23 22:27:10 +0800

    feat: 完成pickerView的显示和隐藏

--- LayersAnimation/LayersAnimation/CALayerControlsController.swift
+++ LayersAnimation/LayersAnimation/CALayerControlsController.swift
@@ -27,7 +27,7 @@ class CALayerControlsController: UITableViewController {
     
     weak var layerViewController: CALayerViewController!
     
-    enum row: Int {
+    enum Row: Int {
         case contentsGravity, contentsGravityPicker, displayContents, geometryFlipped, hidden, opacity, cornerRadius, borderWidth, borderColor, backgroundColor, shadowOpacity, shadowOffset, shadowRadius, shadowColor, magnificationFilter
     }
     
@@ -50,6 +50,80 @@ class CALayerControlsController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        contentsGravityPicker.delegate = self
+        contentsGravityPicker.dataSource = self
+    }
+}
+
+extension CALayerControlsController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let row = Row(rawValue: indexPath.row)
+        if row == .contentsGravityPicker {
+            return contentsGravityPickerVisible ? 162.0 : 0.0
+        }
+        return 44.0
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        defer {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+        let row = Row(rawValue: indexPath.row)!
+        switch row {
+        case .contentsGravity where !contentsGravityPickerVisible:
+            showContentsGravityPicker()
+        default:
+            hideContentsGravityPicker()
+        }
+        
+    }
+    
+    func showContentsGravityPicker() {
+        contentsGravityPickerVisible = true
+        relayoutTableViewCells()
+        let index = contentsGravityValues.index(of: layerViewController.layer.contentsGravity)
+        contentsGravityPicker.selectRow(index, inComponent: 0, animated: false)
+        contentsGravityPicker.isHidden = false
+        contentsGravityPicker.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { [unowned self] in
+            self.contentsGravityPicker.alpha = 1.0
+        }
+    }
+    
+    func hideContentsGravityPicker() {
+        if contentsGravityPickerVisible {
+            tableView.isUserInteractionEnabled = false
+            contentsGravityPickerVisible = false
+            relayoutTableViewCells()
+            
+            UIView.animate(withDuration: 0.25, animations: { [unowned self] in
+                self.contentsGravityPicker.alpha = 0
+            }, completion: { [unowned self] (_) in
+                self.contentsGravityPicker.isHidden = true
+                self.tableView.isUserInteractionEnabled = true
+            })
+        }
+    }
+    
+    func relayoutTableViewCells() {
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+}
+
+extension CALayerControlsController: UIPickerViewDelegate,UIPickerViewDataSource {
+    @available(iOS 2.0, *)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return contentsGravityValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return contentsGravityValues[row] as? String
     }
 }
