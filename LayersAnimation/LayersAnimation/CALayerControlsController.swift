36e59624278d793229b27a7d437eeacf07a44e8d
Mazy <mazy_ios@163.com>
2017-05-24 11:37:10 +0800

    feat: 完成 CALayer 属性的模块

--- LayersAnimation/LayersAnimation/CALayerControlsController.swift
+++ LayersAnimation/LayersAnimation/CALayerControlsController.swift
@@ -32,18 +32,18 @@ class CALayerControlsController: UITableViewController {
     }
     
     enum Switch: Int {
-        case opacity, cornerRadius, borderWidth, shadowOpacity, shadowRadius
+        case displayContents, geometryFlipped, hidden
     }
     
-    enum ColorSlider: Int {
-        case red, green, blue
+    enum Slider: Int {
+        case opacity, cornerRadius, borderWidth, shadowOpacity, shadowRadius
     }
     
     enum MagnificationFilter: Int {
         case linear, nearest, trilinear
     }
     
-    var contentsGravityValues = [kCAGravityCenter, kCAGravityTop, kCAGravityBottom, kCAGravityLeft, kCAGravityRight, kCAGravityTopLeft, kCAGravityTopRight, kCAGravityBottomLeft, kCAGravityBottomRight, kCAGravityResize, kCAGravityResizeAspect, kCAGravityResizeAspectFill] as NSArray
+    var contentsGravityValues = [kCAGravityCenter, kCAGravityTop, kCAGravityBottom, kCAGravityLeft, kCAGravityRight, kCAGravityTopLeft, kCAGravityTopRight, kCAGravityBottomLeft, kCAGravityBottomRight, kCAGravityResize, kCAGravityResizeAspect, kCAGravityResizeAspectFill] as [String]
     
     var contentsGravityPickerVisible = false
     
@@ -52,7 +52,140 @@ class CALayerControlsController: UITableViewController {
 
         contentsGravityPicker.delegate = self
         contentsGravityPicker.dataSource = self
+        
+        updateSliderValueLabels()
     }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        // 获取点击的 switch 的索引
+        guard let theIndex = switches.index(of: sender),
+             // 获取当前的 switch 的位置
+             let theSwitch = Switch(rawValue: theIndex) else { return }
+        switch theSwitch {
+        case .displayContents:
+            layerViewController.layer.contents = sender.isOn ? layerViewController.iconImage : nil
+        case .geometryFlipped:
+            layerViewController.layer.isGeometryFlipped = sender.isOn
+        case .hidden:
+            layerViewController.layer.isHidden = sender.isOn
+        }
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        // 获取索引
+        guard let theIndex = sliders.index(of: sender),
+             // 获取当前 slider 的位置
+             let theSlider = Slider(rawValue: theIndex) else { return }
+        switch theSlider {
+        case .opacity:
+            layerViewController.layer.opacity = sender.value
+        case .cornerRadius:
+            layerViewController.layer.cornerRadius = CGFloat(sender.value)
+        case .borderWidth:
+            layerViewController.layer.borderWidth = CGFloat(sender.value)
+        case .shadowOpacity:
+            layerViewController.layer.shadowOpacity = sender.value
+        case .shadowRadius:
+            layerViewController.layer.shadowRadius = CGFloat(sender.value)
+        }
+        
+        // 修改 label 的值
+        updateSliderValueLabel(theSlider)
+    }
+    
+    func updateSliderValueLabels() {
+        for slider in Slider.opacity.rawValue...Slider.shadowOpacity.rawValue {
+            updateSliderValueLabel(Slider(rawValue: slider)!)
+        }
+    }
+    
+    func updateSliderValueLabel(_ sliderEnum: Slider) {
+        let index = sliderEnum.rawValue
+        let label = sliderValueLabels[index]
+        let slider = sliders[index]
+        
+        switch sliderEnum {
+            case .opacity, .shadowOpacity:
+                label.text = String(format: "%.2f", slider.value)
+            case .borderWidth, .cornerRadius, .shadowRadius:
+                label.text = "\(Int(slider.value))"
+        }
+    }
+    
+    @IBAction func borderColorSliderValueChanged(_ sender: UISlider) {
+        let colorAndLabelText = colorAndLabelForSliders(borderColorSliders)
+        layerViewController.layer.borderColor = colorAndLabelText.color
+        borderColorSlidersValueLabel.text = colorAndLabelText.labelText
+    }
+    
+    @IBAction func backgroundColorSliderValueChanged(_ sender: UISlider) {
+        let colorAndLabelText = colorAndLabelForSliders(backgroundColorSliders)
+        layerViewController.layer.backgroundColor = colorAndLabelText.color
+        backgroundColorSlidersValueLabel.text = colorAndLabelText.labelText
+    }
+    
+    @IBAction func shadowOffsetSliderValueChanged(_ sender: UISlider) {
+        var width: CGFloat = 0
+        var height: CGFloat = 0
+        for slider in shadowOffsetSliders {
+            switch slider.tag {
+            case 101:
+                width = CGFloat(slider.value)
+            case 102:
+                height = CGFloat(slider.value)
+            default:
+                break
+            }
+        }
+        layerViewController.layer.shadowOffset = CGSize(width: width, height: height)
+        shadowOffsetSlidersValueLabel.text = "Width: \(Int(width)), Height: \(Int(height))"
+    }
+    
+    
+    
+    @IBAction func shadowColorSliderValueChanged(_ sender: UISlider) {
+        let colorAndLabelText = colorAndLabelForSliders(shadowColorSliders)
+        layerViewController.layer.shadowColor = colorAndLabelText.color
+        shadowColorSlidersValueLabel.text = colorAndLabelText.labelText
+    }
+    
+    @IBAction func magnificationSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+        let filter = MagnificationFilter(rawValue: sender.selectedSegmentIndex)!
+        var filterValue = ""
+        switch filter {
+        case .linear:
+            filterValue = kCAFilterLinear
+        case .nearest:
+            filterValue = kCAFilterNearest
+        case .trilinear:
+            filterValue = kCAFilterTrilinear
+        }
+        layerViewController.layer.magnificationFilter = filterValue
+    }
+    
+    func colorAndLabelForSliders(_ sliders: [UISlider]) -> (color: CGColor, labelText: String) {
+        
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        for slider in sliders {
+            switch slider.tag {
+            case 101:
+                red = CGFloat(slider.value)
+            case 102:
+                green = CGFloat(slider.value)
+            case 103:
+                blue = CGFloat(slider.value)
+            default: break
+            }
+        }
+        let color = UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1.0).cgColor
+        let labelText = "RGB: \(Int(red)), \(Int(green)), \(Int(blue))"
+        return (color, labelText)
+    }
+    
+    
+    
 }
 
 extension CALayerControlsController {
@@ -83,7 +216,7 @@ extension CALayerControlsController {
         contentsGravityPickerVisible = true
         relayoutTableViewCells()
         let index = contentsGravityValues.index(of: layerViewController.layer.contentsGravity)
-        contentsGravityPicker.selectRow(index, inComponent: 0, animated: false)
+        contentsGravityPicker.selectRow(index!, inComponent: 0, animated: false)
         contentsGravityPicker.isHidden = false
         contentsGravityPicker.alpha = 0
         
@@ -124,6 +257,11 @@ extension CALayerControlsController: UIPickerViewDelegate,UIPickerViewDataSource
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return contentsGravityValues[row] as? String
+        return contentsGravityValues[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        layerViewController.layer.contentsGravity = contentsGravityValues[row]
+        contentsGravityPickerValueLabel.text = layerViewController.layer.contentsGravity
     }
 }
