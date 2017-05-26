e0b2ac228ca1f646e737af2b528bc2c88326c520
Mazy <mazy_ios@163.com>
2017-05-26 15:21:12 +0800

    feat: 完成 CAGradientLayer 布局

---
+++ LayersAnimation/LayersAnimation/CAGradientLayerController.swift
@@ -0,0 +1,25 @@
+//
+//  CAGradientLayerController.swift
+//  LayersAnimation
+//
+//  Created by Mazy on 2017/5/26.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class CAGradientLayerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
