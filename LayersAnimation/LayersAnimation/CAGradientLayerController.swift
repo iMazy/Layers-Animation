4bd661f3eff8b8905061d8d163003716faaec2c1
Mazy <mazy_ios@163.com>
2017-05-26 16:13:53 +0800

    feat: 完成CAGradientLayer 功能展示

--- LayersAnimation/LayersAnimation/CAGradientLayerController.swift
+++ LayersAnimation/LayersAnimation/CAGradientLayerController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class CAGradientLayerController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var viewForGradientLayer: UIView!
+    @IBOutlet weak var startLabel: UILabel!
+    @IBOutlet weak var endLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var orangeLabel: UILabel!
+    @IBOutlet weak var yellowLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var indigoLabel: UILabel!
+    @IBOutlet weak var violetLabel: UILabel!
+    
+    enum Direction: Int {
+        case start, end
     }
     
+    enum color: Int {
+        case red, orange, yellow, green, blue, indigo, violet
+    }
+    
+    let gradientLayer = CAGradientLayer()
+    
+    func setupGradientLayer() {
+        gradientLayer.frame = viewForGradientLayer.bounds
+        gradientLayer.colors = [UIColor.red.cgColor,UIColor.green.cgColor]
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupGradientLayer()
+        viewForGradientLayer.layer.addSublayer(gradientLayer)
+    }
+
+    @IBAction func startAndEedSliderValueChanged(_ sender: UISlider) {
+        switch sender.tag {
+        case Direction.start.rawValue:
+            gradientLayer.startPoint = CGPoint(x: Double(sender.value), y: 0.0)
+        default:
+            gradientLayer.endPoint = CGPoint(x: Double(sender.value), y: 1.0)
+        }
+    }
+    
+    @IBAction func colorsSwitchValueChanged(_ sender: UISwitch) {
+    }
+    
+    @IBAction func colorSliderValueChanged(_ sender: UISlider) {
+    }
 
 }
